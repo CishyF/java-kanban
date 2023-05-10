15bdedd8c093461bc88a47e92cdca6b3be204e98
Cishy <fedorovnikita04@yandex.ru>
2023-05-10 23:48:18 +0300

    fix: получение InMemoryHistoryManager через статический фабричный метод вынесено в конструктор InMemoryTaskManager

--- src/ru/yandex/fedorov/kanban/service/InMemoryTaskManager.java
+++ src/ru/yandex/fedorov/kanban/service/InMemoryTaskManager.java
@@ -15,12 +15,13 @@ public class InMemoryTaskManager implements TaskManager {
     private final Map<Integer, Task> tasks;
     private final Map<Integer, Epic> epics;
     private final Map<Integer, Subtask> subtasks;
-    private final HistoryManager history = Managers.getDefaultHistory();
+    private final HistoryManager history;
 
     public InMemoryTaskManager() {
         tasks = new HashMap<>();
         epics = new HashMap<>();
         subtasks = new HashMap<>();
+        history = Managers.getDefaultHistory();
     }
 
     @Override
