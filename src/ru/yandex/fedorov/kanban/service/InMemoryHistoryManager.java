f43c2b6ab185d48f41e3f816f38749b3fae8f7db
Cishy <fedorovnikita04@yandex.ru>
2023-06-18 22:44:31 +0300

    fix: исправлена структура InMemoryHistoryManager, он соединен с внутренним классом CustomLinkedList

--- src/ru/yandex/fedorov/kanban/service/InMemoryHistoryManager.java
+++ src/ru/yandex/fedorov/kanban/service/InMemoryHistoryManager.java
@@ -5,93 +5,90 @@ import ru.yandex.fedorov.kanban.model.Task;
 import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Objects;
 
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private final CustomLinkedList history;
-    private final HashMap<Integer, CustomLinkedList.Node> nodeById;
+    private final HashMap<Integer, Node> nodeById;
+    private Node first = null;
+    private Node last = null;
 
     public InMemoryHistoryManager() {
-        history = new CustomLinkedList();
         nodeById = new HashMap<>();
     }
 
     @Override
     public void add(Task task) {
-        if (!nodeById.containsKey(task.getId())) {
-            nodeById.put(task.getId(), history.addLast(task));
-        }
+        Objects.requireNonNull(task);
+
+        removeNodeById(task.getId());
+        addLast(task);
+
+        nodeById.put(task.getId(), last);
     }
 
     @Override
     public void remove(int id) {
-        if (nodeById.containsKey(id)) {
-            history.removeNode(nodeById.get(id));
-        }
+        removeNodeById(id);
     }
 
     @Override
     public List<Task> getHistory() {
-        return history.asTasksList();
+        return asTasksList();
     }
 
-    private static class CustomLinkedList {
+    private static class Node {
 
-        private Node first = null;
-        private Node last = null;
+        Node prev;
+        Task data;
+        Node next;
 
-        private static class Node {
-
-            Node prev;
-            Task data;
-            Node next;
-
-            Node(Node prev, Task data, Node next) {
-                this.prev = prev;
-                this.data = data;
-                this.next = next;
-            }
+        Node(Node prev, Task data, Node next) {
+            this.prev = prev;
+            this.data = data;
+            this.next = next;
         }
-
-        Node addLast(Task task) {
-            Node tempNode;
-
-            if (first == null) {
-                tempNode = new Node(null, task, null);
-                first = tempNode;
-                last = tempNode;
-            } else {
-                tempNode = new Node(last, task, null);
-                last.next = tempNode;
-                last = tempNode;
-            }
-
-            return tempNode;
-        }
-
-        List<Task> asTasksList() {
-            List<Task> tasks = new ArrayList<>();
-
-            for (Node elem = first; elem != null; elem = elem.next) {
-                tasks.add(elem.data);
-            }
-
-            return tasks;
-        }
-
-        void removeNode(Node node) {
-            if (node == first) {
-                first = node.next;
-                first.prev = null;
-            } else if (node == last) {
-                last = node.prev;
-                last.next = null;
-            } else {
-                node.next.prev = node.prev;
-                node.prev.next = node.next;
-            }
-        }
-
     }
+
+    private void addLast(Task task) {
+        Node tempNode;
+
+        if (first == null) {
+            tempNode = new Node(null, task, null);
+            first = tempNode;
+        } else {
+            tempNode = new Node(last, task, null);
+            last.next = tempNode;
+        }
+        last = tempNode;
+    }
+
+    private List<Task> asTasksList() {
+        List<Task> tasks = new ArrayList<>();
+
+        for (Node elem = first; elem != null; elem = elem.next) {
+            tasks.add(elem.data);
+        }
+
+        return tasks;
+    }
+
+    private void removeNodeById(int id) {
+        final Node tempNode = nodeById.remove(id);
+
+        if (tempNode == null) {
+            return;
+        } else if (tempNode == first) {
+            first = tempNode.next;
+            first.prev = null;
+        } else if (tempNode == last) {
+            last = tempNode.prev;
+            last.next = null;
+        } else {
+            tempNode.next.prev = tempNode.prev;
+            tempNode.prev.next = tempNode.next;
+        }
+    }
+
 }
