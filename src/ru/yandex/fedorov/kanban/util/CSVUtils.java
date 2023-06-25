95717c156115ae99e0665d3b10154314db5f2145
Cishy <fedorovnikita04@yandex.ru>
2023-06-25 14:27:06 +0300

    feat: добавлены методы для чтения и записи истории просмотров

--- src/ru/yandex/fedorov/kanban/util/CSVUtils.java
+++ src/ru/yandex/fedorov/kanban/util/CSVUtils.java
@@ -1,7 +1,30 @@
 package ru.yandex.fedorov.kanban.util;
 
+import ru.yandex.fedorov.kanban.service.HistoryManager;
+import ru.yandex.fedorov.kanban.service.TaskManager;
+
+import java.util.stream.Collectors;
+
 public final class CSVUtils {
 
     private CSVUtils() {}
 
+    public static String historyToString(HistoryManager history) {
+        return history.getHistory()
+                        .stream()
+                        .map(t -> String.valueOf(t.getId()))
+                        .collect(Collectors.joining(","));
+    }
+
+    public static void addHistoryInManagerFromString(String history, TaskManager taskManager) {
+        for (String tempId : history.split(",")) {
+            int id = Integer.parseInt(tempId);
+
+            if (taskManager.getTask(id) == null) {
+                if (taskManager.getEpic(id) == null) {
+                    taskManager.getSubtask(id);
+                }
+            }
+        }
+    }
 }
