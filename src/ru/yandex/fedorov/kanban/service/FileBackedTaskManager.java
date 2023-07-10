de974fbc0749f6ddc4676d91340e99628f6393a8
Cishy <fedorovnikita04@yandex.ru>
2023-07-10 14:33:27 +0300

    feat: переопределены методы модификации для FileBackedTaskManager

--- src/ru/yandex/fedorov/kanban/service/FileBackedTaskManager.java
+++ src/ru/yandex/fedorov/kanban/service/FileBackedTaskManager.java
@@ -1,11 +1,113 @@
 package ru.yandex.fedorov.kanban.service;
 
+import ru.yandex.fedorov.kanban.model.Epic;
+import ru.yandex.fedorov.kanban.model.Subtask;
 import ru.yandex.fedorov.kanban.model.Task;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class FileBackedTaskManager extends InMemoryTaskManager implements TaskManager {
+
+    @Override
+    public int createTask(Task task) {
+        int id = super.createTask(task);
+        save();
+        return id;
+    }
+
+    @Override
+    public int createEpic(Epic epic) {
+        int id = super.createEpic(epic);
+        save();
+        return id;
+    }
+
+    @Override
+    public int createSubtask(Subtask subtask) {
+        int id = super.createSubtask(subtask);
+        save();
+        return id;
+    }
+
+    @Override
+    public void updateTask(Task task) {
+        super.updateTask(task);
+        save();
+    }
+
+    @Override
+    public void updateEpic(Epic epic) {
+        super.updateEpic(epic);
+        save();
+    }
+
+    @Override
+    public void updateSubtask(Subtask subtask) {
+        super.updateSubtask(subtask);
+        save();
+    }
+
+    @Override
+    public Task getTask(int id) {
+        Task task = super.getTask(id);
+        save();
+        return task;
+    }
+
+    @Override
+    public Epic getEpic(int id) {
+        Epic epic = super.getEpic(id);
+        save();
+        return epic;
+    }
+
+    @Override
+    public Subtask getSubtask(int id) {
+        Subtask subtask = super.getSubtask(id);
+        save();
+        return subtask;
+    }
+
+    @Override
+    public void removeTasks() {
+        super.removeTasks();
+        save();
+    }
+
+    @Override
+    public void removeEpics() {
+        super.removeEpics();
+        save();
+    }
+
+    @Override
+    public void removeSubtasks() {
+        super.removeSubtasks();
+        save();
+    }
+
+    @Override
+    public void removeTask(int id) {
+        super.removeTask(id);
+        save();
+    }
+
+    @Override
+    public void removeEpic(int id) {
+        super.removeEpic(id);
+        save();
+    }
+
+    @Override
+    public void removeSubtask(int id) {
+        super.removeSubtask(id);
+        save();
+    }
+
+    private static void save() {
+
+    }
 
     private static String historyToString(List<Task> history) {
         return history.stream()
