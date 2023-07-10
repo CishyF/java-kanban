87aa9ff90f11883da6ec9b055a354dd60d924c7f
Cishy <fedorovnikita04@yandex.ru>
2023-07-10 14:00:21 +0300

    fix: перенесены метода для работы с файлом в FileBackedTaskManager

--- src/ru/yandex/fedorov/kanban/service/FileBackedTaskManager.java
+++ src/ru/yandex/fedorov/kanban/service/FileBackedTaskManager.java
@@ -1,5 +1,28 @@
 package ru.yandex.fedorov.kanban.service;
 
+import ru.yandex.fedorov.kanban.model.Task;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FileBackedTaskManager extends InMemoryTaskManager implements TaskManager {
+
+    private static String historyToString(List<Task> history) {
+        return history.stream()
+                .map(t -> String.valueOf(t.getId()))
+                .collect(Collectors.joining(","));
+    }
+
+    private static void addHistoryInManagerFromString(String history, TaskManager taskManager) {
+        for (String tempId : history.split(",")) {
+            int id = Integer.parseInt(tempId);
+
+            if (taskManager.getTask(id) == null) {
+                if (taskManager.getEpic(id) == null) {
+                    taskManager.getSubtask(id);
+                }
+            }
+        }
+    }
 
 }
