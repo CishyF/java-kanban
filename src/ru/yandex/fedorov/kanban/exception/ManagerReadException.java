2420464a186f45d16e33e27ff5be07312364f795
Cishy <fedorovnikita04@yandex.ru>
2023-07-11 00:32:41 +0300

    feat: добавлены конструкторы для исключений

--- src/ru/yandex/fedorov/kanban/exception/ManagerReadException.java
+++ src/ru/yandex/fedorov/kanban/exception/ManagerReadException.java
@@ -1,4 +1,9 @@
 package ru.yandex.fedorov.kanban.exception;
 
 public class ManagerReadException extends RuntimeException {
+
+    public ManagerReadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
 }
--- src/ru/yandex/fedorov/kanban/exception/ManagerSaveException.java
+++ src/ru/yandex/fedorov/kanban/exception/ManagerSaveException.java
@@ -1,4 +1,13 @@
 package ru.yandex.fedorov.kanban.exception;
 
 public class ManagerSaveException extends RuntimeException {
+
+    public ManagerSaveException(String message) {
+        super(message);
+    }
+
+    public ManagerSaveException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
 }
