eddeccb7f06701115968c4eca7537c225240f61c
Cishy <fedorovnikita04@yandex.ru>
2023-07-10 14:46:26 +0300

    fix: ManagerSaveException сделано непроверяемым

--- src/ru/yandex/fedorov/kanban/exception/ManagerSaveException.java
+++ src/ru/yandex/fedorov/kanban/exception/ManagerSaveException.java
@@ -1,6 +1,4 @@
 package ru.yandex.fedorov.kanban.exception;
 
-import java.io.IOException;
-
-public class ManagerSaveException extends IOException {
+public class ManagerSaveException extends RuntimeException {
 }
