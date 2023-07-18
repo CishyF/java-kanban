6651ba7af1a7ab8e46f44469c13eb02665169e5c
Cishy <fedorovnikita04@yandex.ru>
2023-07-18 18:21:33 +0300

    feat: тест на корректную работу со временем для Epic-задачи

--- test/ru/yandex/fedorov/kanban/service/FileBackedTaskManagerTest.java
+++ test/ru/yandex/fedorov/kanban/service/FileBackedTaskManagerTest.java
@@ -3,9 +3,11 @@ package ru.yandex.fedorov.kanban.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.fedorov.kanban.model.Epic;
+import ru.yandex.fedorov.kanban.model.Subtask;
 import ru.yandex.fedorov.kanban.model.TaskStatus;
 
 import java.io.File;
+import java.time.LocalDateTime;
 import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -40,5 +42,36 @@ public class FileBackedTaskManagerTest extends TaskManagerTest<FileBackedTaskMan
         assertEquals(epicId, epicFromFile.getId());
         assertEquals(epic, epicFromFile);
         assertEquals(Collections.singletonList(epic), newTaskManager.getHistory());
+    }
+
+    @Test
+    public void shouldBeSameEpicAfterDeserialization() {
+        final int epicId = taskManager.createEpic(
+            new Epic("Название эпика", " ", TaskStatus.NEW)
+        );
+
+        final LocalDateTime startTimeFirstSubtask = LocalDateTime.now();
+        taskManager.createSubtask(
+            new Subtask("Название позадачи 1 эпика", " ", TaskStatus.DONE, startTimeFirstSubtask, 10, epicId)
+        );
+
+        assertEquals(taskManager.getEpic(epicId).getStartTime(), startTimeFirstSubtask);
+        assertEquals(taskManager.getEpic(epicId).getDuration(), 10);
+        assertEquals(taskManager.getEpic(epicId).getEndTime(), startTimeFirstSubtask.plusMinutes(10));
+
+        final LocalDateTime startTimeSecondSubtask = LocalDateTime.now().plusMinutes(20);
+        taskManager.createSubtask(
+            new Subtask("Название позадачи 2 эпика", " ", TaskStatus.DONE, startTimeSecondSubtask, 10, epicId)
+        );
+
+        assertEquals(taskManager.getEpic(epicId).getStartTime(), startTimeFirstSubtask);
+        assertEquals(taskManager.getEpic(epicId).getDuration(), 20);
+        assertEquals(taskManager.getEpic(epicId).getEndTime(), startTimeSecondSubtask.plusMinutes(10));
+
+        final Epic epic = taskManager.getEpic(epicId);
+
+        final TaskManager newTaskManager = FileBackedTaskManager.loadFromFile(new File("resources/data.csv"));
+
+        assertEquals(epic, newTaskManager.getEpic(epicId));
     }
 }
