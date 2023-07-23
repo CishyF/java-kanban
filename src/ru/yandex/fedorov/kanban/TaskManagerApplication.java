7fd9214b787eb4ac3593ccd423e8b4ac43986c86
Cishy <fedorovnikita04@yandex.ru>
2023-07-23 09:55:24 +0300

    feat: исправлен метод main

--- src/ru/yandex/fedorov/kanban/TaskManagerApplication.java
+++ src/ru/yandex/fedorov/kanban/TaskManagerApplication.java
@@ -1,70 +1,19 @@
 package ru.yandex.fedorov.kanban;
 
-import ru.yandex.fedorov.kanban.service.FileBackedTaskManager;
-import ru.yandex.fedorov.kanban.util.Managers;
-import ru.yandex.fedorov.kanban.model.Epic;
-import ru.yandex.fedorov.kanban.model.Subtask;
-import ru.yandex.fedorov.kanban.model.Task;
-import ru.yandex.fedorov.kanban.model.TaskStatus;
-import ru.yandex.fedorov.kanban.service.TaskManager;
+import ru.yandex.fedorov.kanban.web.server.HttpTaskServer;
+import ru.yandex.fedorov.kanban.web.server.KVServer;
 
-import java.io.File;
+import java.io.IOException;
 
 public class TaskManagerApplication {
 
     public static void main(String[] args) {
-        new TaskManagerApplication().start();
-    }
-
-    public void start() {
-        final TaskManager taskManager = Managers.getDefault();
-
-        int idTask1 = taskManager.createTask(
-                new Task("Название 1 задачи", " ", TaskStatus.NEW)
-        );
-
-        int idTask2 = taskManager.createTask(
-                new Task("Название 2 задачи", " ", TaskStatus.IN_PROGRESS)
-        );
-
-        int idEpic1 = taskManager.createEpic(
-                new Epic("Название 1 эпика", " ", TaskStatus.NEW)
-        );
-        int idSubtask1Epic1 = taskManager.createSubtask(
-                new Subtask("Название позадачи 1 эпика 1", " ", TaskStatus.IN_PROGRESS, idEpic1)
-        );
-
-        int idEpic2 = taskManager.createEpic(
-                new Epic("Название 2 эпика", " ", TaskStatus.IN_PROGRESS)
-        );
-        int idSubtask1Epic2 = taskManager.createSubtask(
-                new Subtask("Название подзадачи 1 эпика 2", " ", TaskStatus.NEW, idEpic2)
-        );
-        int idSubtask2Epic2 = taskManager.createSubtask(
-                new Subtask("Название подзадачи 2 эпика 2", " ", TaskStatus.NEW, idEpic2)
-        );
-
-        taskManager.getTask(idTask1);
-        taskManager.getEpic(idEpic2);
-        taskManager.getSubtask(idSubtask1Epic1);
-
-        printInfo(taskManager);
-
-        File file = new File("resources/data.csv");
-        FileBackedTaskManager newManager = FileBackedTaskManager.loadFromFile(file);
-
-        printInfo(newManager);
-
-    }
-
-    public void printInfo(TaskManager taskManager) {
-        System.out.printf("Задачи:%n%s%nЭпики:%n%s%nПодзадачи:%n%s%nИстория:%n%s%n%s%n",
-                taskManager.getTasks(),
-                taskManager.getEpics(),
-                taskManager.getSubtasks(),
-                taskManager.getHistory(),
-                "-".repeat(200)
-        );
+        try {
+            new KVServer().start();
+            new HttpTaskServer().start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
