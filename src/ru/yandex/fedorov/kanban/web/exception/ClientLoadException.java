4e8fd6f21b55d6c3ee653833e087aa77ab191883
Cishy <fedorovnikita04@yandex.ru>
2023-07-23 09:52:07 +0300

    feat: добавлен новый конструктор

--- src/ru/yandex/fedorov/kanban/web/exception/ClientLoadException.java
+++ src/ru/yandex/fedorov/kanban/web/exception/ClientLoadException.java
@@ -2,6 +2,10 @@ package ru.yandex.fedorov.kanban.web.exception;
 
 public class ClientLoadException extends RuntimeException {
 
+    public ClientLoadException(String message) {
+        super(message);
+    }
+
     public ClientLoadException(String message, Throwable cause) {
         super(message, cause);
     }
